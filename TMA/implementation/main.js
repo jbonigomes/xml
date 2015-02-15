b831943463600e9e2fbca09cc622c114ec7a8970
Jose B Gomes <jbonigomes@yahoo.com.br>
2015-02-15 01:37:54 +0000

    Final touches

--- TMA/implementation/main.js
+++ TMA/implementation/main.js
@@ -2,14 +2,17 @@ $(function($) {
 
   $.get('contacts.xml.txt', function(data) {
     $('#contactsxml').text(data).html();
+    PR.prettyPrint()
   });
 
   $.get('contacts.dtd.txt', function(data) {
     $('#contactsdtd').text(data).html();
+    PR.prettyPrint()
   });
 
   $.get('contacts.xsd.txt', function(data) {
     $('#contactsxsd').text(data).html();
+    PR.prettyPrint()
   });
 
 });
